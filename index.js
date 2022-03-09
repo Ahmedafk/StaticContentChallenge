9b612124c05cf213b7a3c7572b3c9771a3fe0b77
Ahmed Abdulkadir <ahmedaaa_123@hotmail.com>
2022-03-09 20:57:06 +0000

    Format page by processing markdown syntax

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const fs = require('fs')
 const express = require('express')
-const app = express()
-const path = require('path');
-const port = 3000
+const path = require('path')
+const fs = require('fs')
+const hbs = require('hbs')
+const showdown  = require('showdown')
 
-app.engine('html', require('hbs').__express);
+const app = express()
+const port = 3000
+const converter = new showdown.Converter()
+
+app.engine('html', hbs.__express)
 
 app.get('/:url_path(*)', (req, res) => {
     const contentPath = path.join(`${__dirname}/content/${req.params.url_path}/index.md`)
-    console.log('contentPath', contentPath)
 
     let content
     try {
+        // Read file content and store as a string
         content = fs.readFileSync(contentPath, 'utf8')
+
+        // Convert markdown in string to HTML
+        content = converter.makeHtml(content)
     } catch (error) {
+        // Placeholder in case url_path does not have content
         content = '<h2>Error 404</h2><p>Page not found</p>'
         res.status(404)
     }
